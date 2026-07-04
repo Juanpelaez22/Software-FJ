87adee3a8f5daf7c96831f31f98d436f6bd36181
Juan Camilo <juanpelaez22@gmail.com>
2026-07-04 17:26:24 -0500

    Initial project structure

---
+++ Software_FJ.py
@@ -0,0 +1,59 @@
+# Student: Juan Camilo Peláez Feged
+# Grupo: 213023_37
+# SOFTWARE FJ - Customer Management System
+
+
+
+# IMPORTS
+
+...
+
+# LOGGER
+
+...
+
+# CUSTOM EXCEPTIONS
+
+...
+
+# ABSTRACT CLASS
+
+...
+
+# CUSTOMER CLASS
+
+...
+
+# SERVICE CLASS
+
+...
+
+# ROOM RESERVATION
+
+...
+
+# EQUIPMENT RENTAL
+
+...
+
+# CONSULTING SERVICE
+
+...
+
+# RESERVATION CLASS
+
+...
+
+# LISTS
+
+...
+
+# FUNCTIONS
+
+...
+
+# TKINTER INTERFACE
+
+...
+
+# MAIN
